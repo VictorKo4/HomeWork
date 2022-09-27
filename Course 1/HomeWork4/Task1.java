7f98275494db61502df31136075c6865b59eb0e7
VictorKo4 <kochnak@mail.ru>
2022-09-27 18:32:07 +0300

    HomeWork 4 Task1 1, 2, 3

--- Course 1/HomeWork4/Task1.java
+++ Course 1/HomeWork4/Task1.java
@@ -3,15 +3,11 @@ package HomeWork4;
 public class Task1 {
     public static void main(String[] args) {
         int age = 18;
-        if (age < 18) {
-        } else {
-            System.out.println("Поздравляем! Вам исполнилось " + age + " лет.");
-        }
-
-        age = 17;
-        if (age >= 18) {
-        } else {
+        if (age <= 17) {
             System.out.println("Ваш возраст " + age + " лет. Нужно еще немного подождать =)");
+        }
+        if (age >= 18) {
+            System.out.println("Поздравляем! Вам исполнилось " + age + " лет.");
         }
     }
 }
--- Course 1/HomeWork4/Task3.java
+++ Course 1/HomeWork4/Task3.java
@@ -5,24 +5,19 @@ public class Task3 {
         int trainWagonCapacity = 102;
         int wagonSeating       = 60;
         int wagonStanding      = trainWagonCapacity - wagonSeating;
-        int totalPass          = 42;
-        if (totalPass > wagonSeating) {
-        } else {
+        int totalPass          = 102;
+        if (totalPass < wagonSeating) {
             System.out.println("Если в вагон зайдет " + totalPass +
-                    " человека, то сидячих мест останется " + (wagonSeating - totalPass));
+                " человека, то сидячих мест останется " + (wagonSeating - totalPass));
         }
-        totalPass = 60;
-        if (totalPass > wagonSeating) {
-        } else {
+        if (totalPass >= wagonSeating) {
+                if (totalPass < trainWagonCapacity)
             System.out.println("Если в вагон зайдет " + totalPass +
-                    " человек, то сидячих мест уже не останется. Останется " +
-                    wagonStanding + " стоячих мест.");
+                " человек, то останется " + (wagonSeating - (totalPass - wagonStanding)) + " стоячих места.");
         }
-        totalPass = 102;
-        if (totalPass > trainWagonCapacity) {
-        } else {
-        System.out.println("Если в вагон зайдет " + totalPass +
-                " человека, то мест в вагоне не останется.");
+        if (totalPass >= trainWagonCapacity) {
+            System.out.println("Если в вагон зайдет " + totalPass +
+                    " человека, то мест в вагоне не останется.");
         }
     }
 }
