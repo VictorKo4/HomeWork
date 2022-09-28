b6fd440bac8cb6fd55dc478f049d0aa497be27a3
VictorKo4 <kochnak@mail.ru>
2022-09-28 16:10:50 +0300

    HomeWork

---
+++ Course 1/HomeWork5/Task1.java
@@ -0,0 +1,13 @@
+package HomeWork5;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+}
---
+++ Course 1/HomeWork5/Task2.java
@@ -0,0 +1,17 @@
+package HomeWork5;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int clientDeviceYear = 2015;
+        int clientOS = 1;
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
+        }
+    }
+}
