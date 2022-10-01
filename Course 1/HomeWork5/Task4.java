9da8cdffb3e93324f8c7afa9f70ac63e8facdaa2
VictorKo4 <kochnak@mail.ru>
2022-10-01 11:56:26 +0300

    HomeWork Поправочка =)

--- Course 1/HomeWork5/Task4.java
+++ Course 1/HomeWork5/Task4.java
@@ -2,14 +2,14 @@ package HomeWork5;
 
 public class Task4 {
     public static void main(String[] args) {
-        int deliveryDistance = 60;
+        int deliveryDistance = 1;
         int deliveryDay = 1;
-        if (deliveryDistance < 20) {
+        if (deliveryDistance <= 20) {
             System.out.println("Потребуется дней: " + deliveryDay);
-        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+        } else if (deliveryDistance <= 60) {
             deliveryDay = 2;
             System.out.println("Потребуется дней: " + deliveryDay);
-        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+        } else if (deliveryDistance <= 100) {
             deliveryDay = 3;
             System.out.println("Потребуется дней: " + deliveryDay);
         }
