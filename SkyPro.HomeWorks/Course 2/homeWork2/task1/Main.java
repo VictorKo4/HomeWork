915c0253ebda3117f75fda9d1a61bef499ed4f82
VictorKo4 <kochnak@mail.ru>
2022-11-19 14:10:35 +0300

    HomeWork 2

---
+++ SkyPro.HomeWorks/Course 2/homeWork2/task1/Main.java
@@ -0,0 +1,18 @@
+package homeWork2.task1;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.maxim();
+        person.anna();
+        person.artem();
+        person.kate();
+        person.vladimir();
+
+        Flower.flowers();
+    }
+}
---
+++ SkyPro.HomeWorks/Course 2/homeWork2/task3/Main.java
@@ -0,0 +1,13 @@
+package homeWork2.task3;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("","",0,"",0,"","","","",0,"",new Car.Key("",""),new Car.Insurance("","",""));
+        car.ladaGranta();
+        car.audiA8();
+        car.bmwZ8();
+        car.kiaSportage();
+        car.hyundaiAvante();
+        Car.cars();
+    }
+}
